e2f9bf009b89375ded0247bcd0e49ba9d2b7659f
Lydia Charles <lydiatpcharles@gmail.com>
2023-03-27 19:08:55 -0700

    CampsiteDetailPage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import { Container } from "reactstrap";
+import DisplayList from '../features/display/DisplayList';
+
+
+const HomePage = () => {
+    return (
+        <Container>
+            <DisplayList/>
+        </Container>
+    );
+};
+
+export default HomePage;
